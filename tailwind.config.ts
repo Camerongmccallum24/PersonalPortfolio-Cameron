510d6fc8562ab39d4a2c4518c438e2bb017f684e
Cameron McCallum <Camerongmccallum@outlook.com>
2025-01-27 19:30:26 +0000

    Restored to '45af7c94f2e140da17a90b9e657ab44fdf9bf838'
    Replit-Restored-To:45af7c94f2e140da17a90b9e657ab44fdf9bf838

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -84,9 +84,6 @@ export default {
         "accordion-down": "accordion-down 0.2s ease-out",
         "accordion-up": "accordion-up 0.2s ease-out",
       },
-      scale: {
-        '130': '1.3',
-      },
     },
   },
   plugins: [require("tailwindcss-animate"), require("@tailwindcss/typography")],
